896b8610ea9b5269ec1eead52d7252a7189a7dd4
黃宇新 <astroboy0803@gmail.com>
2023-03-25 13:09:15 +0800

    File Template - MVVM 初版

---
+++ File Templates/MVVM.xctemplate/CombineXIB/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,26 @@
+//___FILEHEADER___
+
+import UIKit
+import Combine
+
+class ___FILEBASENAME___: UIViewController {
+    
+    var viewModel: ___VARIABLE_productName___ViewModel
+    
+    init() {
+        viewModel = .init()
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }    
+}
---
+++ File Templates/MVVM.xctemplate/CombineXIB/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,8 @@
+//___FILEHEADER___
+
+import Foundation
+import Combine
+
+class ___FILEBASENAME___ {
+
+}
